32e53d2a187cc148d73bba9434bcdb80ce73b46f
Daniil Grbić <daniilgrbic@gmail.com>
2024-08-17 18:08:59 +0200

    Add examples for IdenticalBBMergePass

---
+++ IdenticalBBMergePass/examples/BranchingCanMerge.cpp
@@ -0,0 +1,13 @@
+int func(int a, int b) {
+
+    int c = 0;
+
+    if(a > b) {
+        c = a + b;
+    }
+    else {
+        c = a + b;
+    }
+
+    return c;
+}
--- IdenticalBBMergePass/examples/BranchingDoNotMerge.cpp
+++ IdenticalBBMergePass/examples/BranchingDoNotMerge.cpp
@@ -1,12 +1,13 @@
 int func(int a, int b) {
 
     int c = 0;
+    int d = 0;
 
     if(a > b) {
         c = a + b;
     }
     else {
-        c = a + b;
+        d = a + b;
     }
 
     return c;
